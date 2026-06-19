b51bd045cb18933050af8dd619e705d410f511aa
George Dimitropoulos <georgios.dimitropoulos@sthenos.ai>
2026-06-19 10:46:16 +0300

    feat: adds baseline pGenRescue app

---
+++ src/pGenRescue/CMakeFiles/pGenRescue.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../../bin/pGenRescue"
+  "../../bin/pGenRescue.pdb"
+  "CMakeFiles/pGenRescue.dir/GenRescue.cpp.o"
+  "CMakeFiles/pGenRescue.dir/GenRescue.cpp.o.d"
+  "CMakeFiles/pGenRescue.dir/GenRescue_Info.cpp.o"
+  "CMakeFiles/pGenRescue.dir/GenRescue_Info.cpp.o.d"
+  "CMakeFiles/pGenRescue.dir/main.cpp.o"
+  "CMakeFiles/pGenRescue.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pGenRescue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
