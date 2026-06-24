4e8cd76cdd6dd94e2e46957ae53c3a214fcb1005
George Dimitropoulos <georgios.dimitropoulos@sthenos.ai>
2026-06-24 12:20:02 +0300

    feat: Adds baseline Scout behavior to extend tree

---
+++ src/lib_bhv_scout/CMakeFiles/BHV_Scout.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../lib/libBHV_Scout.dylib"
+  "../../lib/libBHV_Scout.pdb"
+  "CMakeFiles/BHV_Scout.dir/BHV_Scout.cpp.o"
+  "CMakeFiles/BHV_Scout.dir/BHV_Scout.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BHV_Scout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/lib_bhv_scout/CMakeFiles/BHV_Scout.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for BHV_Scout.
+# This may be replaced when dependencies are built.
